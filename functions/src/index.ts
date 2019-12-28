cd0496e202f0ebfdf8025011545956279bd37f28
Guedelho <mateus.guedelho@gmail.com>
2019-12-28 13:18:13 -0600

    Add setTodos method; Needs refactor

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,19 +7,71 @@ import * as request from 'request';
 const config = functions.config();
 const { uri, user, key } = config.api;
 
-export const scheduledFunctionCrontab = functions.pubsub.schedule('0 0 * * *')
+export const scheduledFunctionCrontabSetTodos = functions.pubsub.schedule('0 0 * * *')
     .timeZone('US/Central')
-    .onRun((context) => {
-        request.post(`${uri}/tasks/user`)
-        return null;
+    .onRun(() => {
+        const headers = {
+            'x-api-user': user,
+            'x-api-key': key,
+        };
+        const body = {
+            text: '',
+            type: 'todo',
+            priority: 2,
+        }
+        const todos = [
+            'Drink Water',
+            'Make my Bed',
+            'Exercise',
+            'Shower',
+            'Brush my Teeth',
+            'Eat a Great Breakfest',
+            'Break the Habit! The No Sugar Challenge',
+            'Deep Work',
+            'Learn & Study',
+            'Read',
+            'The 16:8 Intermittent Fasting Method',
+        ];
+
+        todos.forEach((todo) => {
+            body.text = todo;
+            request.post(`${uri}/tasks/user`, { headers, body }, (error, response, body) => {
+
+            });
+        });
 });
 
 export const testGet = functions.https.onRequest((req, resp) => {
     const headers = {
         'x-api-user': user,
         'x-api-key': key,
+        'Content-Type': 'application/json',
     };
-    request.get(`${uri}/tags/`, { headers }, (error, response, body) => {
-        resp.send(body);
+    const body = {
+        text: '',
+        type: 'todo',
+        priority: 2,
+    }
+    const todos = [
+        'Drink Water',
+        'Make my Bed',
+        'Exercise',
+        'Shower',
+        'Brush my Teeth',
+        'Eat a Great Breakfest',
+        'Break the Habit! The No Sugar Challenge',
+        'Deep Work',
+        'Learn & Study',
+        'Read',
+        'The 16:8 Intermittent Fasting Method',
+    ];
+
+    todos.forEach((todo) => {
+        body.text = todo;
+        request.post(`${uri}/tasks/user`, { headers, body, json: true }, (error, response, body) => {
+            console.log('error:', error);
+            console.log('statusCode:', response && response.statusCode);
+            console.log('body:', body);
+        });
     });
 });

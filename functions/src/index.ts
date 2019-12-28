c9f541dccfe91bdb75406eb3d5e26fcf9039b284
Guedelho <mateus.guedelho@gmail.com>
2019-12-28 11:57:00 -0600

    Add habitica configs and api call example

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,25 @@
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+import * as request from 'request';
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
+const config = functions.config();
+const { uri, user, key } = config.api;
+
 export const scheduledFunctionCrontab = functions.pubsub.schedule('0 0 * * *')
     .timeZone('US/Central')
     .onRun((context) => {
-        console.log(admin.database.ServerValue.TIMESTAMP);
-    return null;
+        request.post(`${uri}/tasks/user`)
+        return null;
+});
+
+export const testGet = functions.https.onRequest((req, resp) => {
+    const headers = {
+        'x-api-user': user,
+        'x-api-key': key,
+    };
+    request.get(`${uri}/tags/`, { headers }, (error, response, body) => {
+        resp.send(body);
+    });
 });
